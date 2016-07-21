4b9a68ff68d97c3c6648b36601c8be3b70b4a4df
liumeng <liumeng@localhost>
2016-07-21 16:27:16 +0800

    Initial Commit

---
+++ 西邮成绩/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  西邮成绩
+//
+//  Created by liumeng on 16/7/21.
+//  Copyright © 2016年 liumeng. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ 西邮成绩/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  西邮成绩
+//
+//  Created by liumeng on 16/7/21.
+//  Copyright © 2016年 liumeng. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
